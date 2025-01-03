b1316c2f03d2db80563bc351c36cca2f207b0826
Itay Almog <itay2828@gmail.com>
2025-01-03 15:00:54 +0200

    forgot to write ctors for this

--- System.Private.CoreLib/System/Diagnostics/DebuggableAttribute.cs
+++ System.Private.CoreLib/System/Diagnostics/DebuggableAttribute.cs
@@ -14,4 +14,30 @@ public sealed class DebuggableAttribute : Attribute
         EnableEditAndContinue = 0x4
     }
     
+    public bool IsJITTrackingEnabled => (DebuggingFlags & DebuggingModes.Default) != 0;
+
+    public bool IsJITOptimizerDisabled => (DebuggingFlags & DebuggingModes.DisableOptimizations) != 0;
+
+    public DebuggingModes DebuggingFlags { get; }
+    
+    public DebuggableAttribute(bool isJITTrackingEnabled, bool isJITOptimizerDisabled)
+    {
+        DebuggingFlags = 0;
+
+        if (isJITTrackingEnabled)
+        {
+            DebuggingFlags |= DebuggingModes.Default;
+        }
+
+        if (isJITOptimizerDisabled)
+        {
+            DebuggingFlags |= DebuggingModes.DisableOptimizations;
+        }
+    }
+    
+    public DebuggableAttribute(DebuggingModes modes)
+    {
+        DebuggingFlags = modes;
+    }
+    
 }

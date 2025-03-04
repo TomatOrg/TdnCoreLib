e794fa77d1bc422fd9d568098e4bafea169ba7f0
Itay Almog <itay2828@gmail.com>
2025-03-05 00:49:10 +0200

    moved stuff around

--- System.Private.CoreLib/Array.cs
+++ System.Private.CoreLib/Array.cs
@@ -6,6 +6,8 @@ public abstract class Array
     private int _length;
     private int _subLength;
     
+    public int Length => _length;
+    
     internal Array()
     {
     }
--- Tests/Program.cs
+++ Tests/Program.cs
@@ -1,10 +1,32 @@
-﻿namespace Tests;
+﻿using System;
+using System.Runtime.CompilerServices;
+
+namespace Tests;
 
 internal static class Program
 {
 
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    private static int ReduceDelegate(int[] a, Func<int,int,int> d)
+    {
+        if (a.Length == 0) return 0;
+
+        var result = a[0];
+        for (int i = 1; i < a.Length; i++) {
+            result = d(result, a[i]);
+        }
+        return result;
+    }
+
+    public static int Add(int a, int b)
+    {
+        return a + b;
+    }
+    
     public static int Main(string[] args)
     {
-        return 123;
+        var a = new Func<int, int, int>(Add);
+        return a(1, 2);
+        // return ReduceDelegate([1,2,3], new Func<int, int, int>(Add));
     }
 }

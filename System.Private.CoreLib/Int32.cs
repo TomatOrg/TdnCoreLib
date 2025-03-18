512b29df4c9b4b846d9d609b45964422b4260ea1
Itay Almog <itay2828@gmail.com>
2025-03-19 00:56:10 +0200

    remove these functions for now

--- System.Private.CoreLib/Int32.cs
+++ System.Private.CoreLib/Int32.cs
@@ -1,40 +1,8 @@
 namespace System;
 
 public readonly struct Int32
-    : IComparable
-    , IComparable<Int32> 
-    , IEquatable<Int32>
 {
-    
-    public const int MaxValue = 0x7fffffff;
-    public const int MinValue = unchecked((int)0x80000000);
     
     private readonly int _value;
     
-    public int CompareTo(object? value)
-    {
-        if (value == null)
-        {
-            return 1;
-        }
-        
-        if (value is int i)
-        {
-            if (this < i) return -1;
-            if (this > i) return 1;
-            return 0;
-        }
-
-        throw new ArgumentException("Object must be of type Int32.");
-    }
-
-    public int CompareTo(int other)
-    {
-        throw new NotImplementedException();
-    }
-
-    public bool Equals(int other)
-    {
-        throw new NotImplementedException();
-    }
 }

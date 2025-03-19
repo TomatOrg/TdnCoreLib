d40c70fb834ad6c476d8bf8c3b26131d32de11df
Itay Almog <itay2828@gmail.com>
2025-03-20 00:06:42 +0200

    needed for tests

--- System.Private.CoreLib/Int16.cs
+++ System.Private.CoreLib/Int16.cs
@@ -3,6 +3,9 @@ namespace System;
 public readonly struct Int16
 {
 
+    public const short MaxValue = (short)0x7FFF;
+    public const short MinValue = unchecked((short)0x8000);
+    
     private readonly short _value;
 
 }
--- System.Private.CoreLib/SByte.cs
+++ System.Private.CoreLib/SByte.cs
@@ -2,7 +2,9 @@ namespace System;
 
 public readonly struct SByte
 {
-    
+    public const sbyte MaxValue = (sbyte)0x7F;
+    public const sbyte MinValue = unchecked((sbyte)0x80);
+
     private readonly sbyte _value;
     
 }

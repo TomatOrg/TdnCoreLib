2831c2e2cab0a2519a362e27a6a4571ce93a6d70
Itay Almog <itay2828@gmail.com>
2025-05-17 17:01:59 +0300

    Need min/max for tests

--- System.Private.CoreLib/Int32.cs
+++ System.Private.CoreLib/Int32.cs
@@ -3,6 +3,9 @@ namespace System;
 public readonly struct Int32
 {
     
+    public const int MaxValue = 0x7fffffff;
+    public const int MinValue = unchecked((int)0x80000000);
+    
     private readonly int _value;
     
     public override string ToString() {

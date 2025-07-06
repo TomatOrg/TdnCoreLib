2b1378afef40a01400c45e248ae9cb1b98461c60
Itay Almog <itay2828@gmail.com>
2025-07-06 22:23:47 +0300

    Added Sqrt to Math, for tests

--- System.Private.CoreLib/Math.cs
+++ System.Private.CoreLib/Math.cs
@@ -4,6 +4,10 @@ namespace System;
 
 public static class Math
 {
+    
+    [MethodImpl(MethodCodeType = MethodCodeType.Native)]
+    public static extern double Sqrt(double d);
+    
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public static double Abs(double value)
     {
